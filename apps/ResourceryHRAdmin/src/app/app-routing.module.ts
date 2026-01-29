da0991b2a2911cd833dff394a1dc909fa8fe1604
Oluropo Akinbolade-Jones <ropojones@gmail.com>
2026-01-29 16:45:17 +0100

    Customize landing page and add job module in ResouceryHR Admin

--- apps/ResourceryHRAdmin/src/app/app-routing.module.ts
+++ apps/ResourceryHRAdmin/src/app/app-routing.module.ts
@@ -25,6 +25,7 @@ const routes: Routes = [
     loadChildren: () =>
       import('@abp/ng.setting-management').then(m => m.SettingManagementModule.forLazy()),
   },
+  { path: 'jobs', loadChildren: () => import('./jobs/jobs.module').then(m => m.JobsModule) },
 ];
 
 @NgModule({
