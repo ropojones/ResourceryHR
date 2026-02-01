78aaee8405bafb86d9e757f77c1b41e56a8ebac1
Oluropo Akinbolade-Jones <ropojones@gmail.com>
2026-02-01 16:04:06 +0100

    Add permissions for Orgaqnization, Exercise and JobPosting

--- services/recruitment/src/ResourceryHR.Recruitment.Application.Contracts/Permissions/RecruitmentPermissionDefinitionProvider.cs
+++ services/recruitment/src/ResourceryHR.Recruitment.Application.Contracts/Permissions/RecruitmentPermissionDefinitionProvider.cs
@@ -8,26 +8,65 @@ public class RecruitmentPermissionDefinitionProvider : PermissionDefinitionProvi
 {
     public override void Define(IPermissionDefinitionContext context)
     {
-        var projectsGroup = context.AddGroup(
+        var recruitmentGroup = context.AddGroup(
             RecruitmentPermissions.GroupName,
             L("Permission:Recruitment")
         );
-        var projectsPermissions = projectsGroup.AddPermission(
-            RecruitmentPermissions.Issues.Default,
-            L("Permission:Recruitment:Issues")
+       
+       //organizations permissions
+        var recruitmentPermissions = recruitmentGroup.AddPermission(
+            RecruitmentPermissions.Organizations.Default,
+            L("Permission:Recruitment:Organizations")
         );
-        projectsPermissions.AddChild(
-            RecruitmentPermissions.Issues.Create,
-            L("Permission:Recruitment:Issues:Create")
+        recruitmentPermissions.AddChild(
+            RecruitmentPermissions.Organizations.Create,
+            L("Permission:Recruitment:Organizations:Create")
         );
-        projectsPermissions.AddChild(
-            RecruitmentPermissions.Issues.Update,
-            L("Permission:Recruitment:Issues:Update")
+        recruitmentPermissions.AddChild(
+            RecruitmentPermissions.Organizations.Update,
+            L("Permission:Recruitment:Organizations:Update")
         );
-        projectsPermissions.AddChild(
-            RecruitmentPermissions.Issues.Delete,
-            L("Permission:Recruitment:Issues:Delete")
+        recruitmentPermissions.AddChild(
+            RecruitmentPermissions.Organizations.Delete,
+            L("Permission:Recruitment:Organizations:Delete")
         );
+
+        //Exercises permissions
+        var exercisePermissions = recruitmentGroup.AddPermission(   
+            RecruitmentPermissions.Exercises.Default,
+            L("Permission:Recruitment:Exercises")
+        );
+        exercisePermissions.AddChild(
+            RecruitmentPermissions.Exercises.Create,
+            L("Permission:Recruitment:Exercises:Create")
+        );
+        exercisePermissions.AddChild(
+            RecruitmentPermissions.Exercises.Update,
+            L("Permission:Recruitment:Exercises:Update")
+        );
+        exercisePermissions.AddChild(
+            RecruitmentPermissions.Exercises.Delete,
+            L("Permission:Recruitment:Exercises:Delete")
+        );
+
+        //JobPostings permissions
+        var jobPostingPermissions = recruitmentGroup.AddPermission(   
+            RecruitmentPermissions.JobPostings.Default,
+            L("Permission:Recruitment:JobPostings")
+        );
+        jobPostingPermissions.AddChild(
+            RecruitmentPermissions.JobPostings.Create,
+            L("Permission:Recruitment:JobPostings:Create")
+        );
+        jobPostingPermissions.AddChild(
+            RecruitmentPermissions.JobPostings.Update,
+            L("Permission:Recruitment:JobPostings:Update")
+        );
+        jobPostingPermissions.AddChild(
+            RecruitmentPermissions.JobPostings.Delete,
+            L("Permission:Recruitment:JobPostings:Delete")
+        );  
+
     }
 
     private static LocalizableString L(string name)
--- services/recruitment/src/ResourceryHR.Recruitment.Application.Contracts/Permissions/RecruitmentPermissions.cs
+++ services/recruitment/src/ResourceryHR.Recruitment.Application.Contracts/Permissions/RecruitmentPermissions.cs
@@ -6,13 +6,30 @@ public class RecruitmentPermissions
 {
     public const string GroupName = "Recruitment";
 
-    public static class Issues
+    public static class Organizations
     {
-        public const string Default = GroupName + ".Issues";
+        public const string Default = GroupName + ".Organizations";
         public const string Create = Default + ".Create";
         public const string Update = Default + ".Update";
         public const string Delete = Default + ".Delete";
     }
+
+    public static class Exercises
+    {
+        public const string Default = GroupName + ".Exercises";
+        public const string Create = Default + ".Create";
+        public const string Update = Default + ".Update";
+        public const string Delete = Default + ".Delete";
+    }
+
+    public static class JobPostings
+    {
+        public const string Default = GroupName + ".JobPostings";
+        public const string Create = Default + ".Create";
+        public const string Update = Default + ".Update";
+        public const string Delete = Default + ".Delete";
+    }
+
 
     public static string[] GetAll()
     {
